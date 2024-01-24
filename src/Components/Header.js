9898630438976bf687ddd46da59237992723adbd
Maksymilian Olejnik <Maxx.olejnik@gmail.com>
2024-01-24 19:41:47 +0100

    header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,11 @@
 import react from "react";
+import Search from "./Search";
 
 function Header(){
     return (
         <header id="header">
             Weather Forcast
+            <Search placeholder="Search"/>
         </header>
     );
 }
---
+++ src/Components/Search.js
@@ -0,0 +1,7 @@
+function Search(props){
+    return (
+        <input className="searchBar" placeholder={props.placeholder} />
+    );
+}
+
+export default Search;
