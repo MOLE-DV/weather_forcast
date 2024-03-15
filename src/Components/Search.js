fce5444f8de4c50e1e3a19f5cc9e7bf733924268
MOLE-DV <maxx.olejnik@gmail.com>
2024-03-15 09:43:05 +0100

    Searchbar hints

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,5 +1,12 @@
 import Underline from "./Underline";
 import cities from '../cities.json';
+import countries from '../countries.json';
+
+
+async function sleep(s){
+    return new Promise(resolve=>setTimeout(resolve, s * 1000));
+}
+
 
 function SearchButton(){
     return (
@@ -10,23 +17,41 @@ function SearchButton(){
     );
 }
 
-async function sleep(s){
-    return new Promise(resolve=>setTimeout(resolve, s * 1000));
+
+function SearchHints(props){
+    return (
+        <div className="hint">
+            {props.text}
+        </div>
+    )
 }
 
+
+
 window.onload = ()=>{
-    document.getElementById("input").addEventListener('keypress', async (e)=>{
-        let hm = 0, cm = 0;
-        let input  = document.getElementById("input").value;
-        cities.forEach((city) =>{
-            input.split('').forEach((letter) =>{
-                console.log(city.name.split('').find((l)=>{
-                    return l == letter;
-                }));
+    document.getElementById("input").addEventListener('keyup', async (e)=>{
+       
+        let input = document.getElementById("input").value;
+        let matches = [];
+
+
+        if(input != ""){
+            cities.forEach((city)=>{
+                if(city.name.toLowerCase().search(input.toLowerCase()) == 0){
+                    matches.push([city.name, city.country]);
+                }
             })
-            console.log(`Highest ${hm}, Current ${cm}`);
-            if(cm > hm) hm = cm;
-        })
+        }
+
+        let hints = []
+
+        document.getElementById('hints').innerHTML = '';
+        for(let i = 0; i < 5; i++){
+            if(matches[i] != undefined) hints.push(`<div class="hint" onClick="localStorage.setItem('cityName', '`+ matches[i][0] + `, `+ matches[i][1] +`'); window.location.reload();">` + matches[i][0] + `, `+ countries[matches[i][1]] + `</div>`);
+        }
+
+        document.getElementById('hints').innerHTML = hints.join(' ');
+
         if(e.keyCode === 13){
             localStorage.setItem("cityName", document.getElementById("input").value);
             let weather = document.getElementById("cur_weather").innerText;
@@ -57,9 +82,12 @@ function cloudsGoRight(){
 function Search(props){
     return (
         <div id="searchBar">
-            <input id="input" placeholder={props.placeholder}/>
-            <SearchButton />
-            <div id="srch_underline"></div>
+            <div id="top">
+                <input id="input" placeholder={props.placeholder} autoComplete="off"/>
+                <SearchButton />
+                <div id="srch_underline"></div>
+            </div>
+            <div id="hints"></div>
         </div>
     );
 }
