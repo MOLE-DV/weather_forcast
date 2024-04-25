c3edc285bc9579cb58966722b34868fa7110b60d
MOLE-DV <maxx.olejnik@gmail.com>
2024-04-25 20:09:00 +0200

    s

--- src/Components/FutureForecast.tsx
+++ src/Components/FutureForecast.tsx
@@ -43,7 +43,7 @@ function Futureforecast(props) {
           {new Date(data.forecastday[0].date).toLocaleString("en-us", {
             weekday: "long",
           })}
-          {iconTable[data.forecastday[0].day.condition.text.toLowerCase()]};
+          {iconTable[data.forecastday[0].day.condition.text.toLowerCase()]}
         </h1>
         <div id="frcst">
           {data &&
@@ -65,7 +65,9 @@ function Futureforecast(props) {
             })}
         </div>
       </div>
-      <div id="down"></div>
+      <div id="down">
+        <div id="city "></div>
+      </div>
     </div>
   );
 }
